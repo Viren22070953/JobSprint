22641fe7232763905a1ca1735b67819127a44e62
Viren Rahangdale <virenrahangdale12@gmail.com>
2026-06-17 22:13:53 +0530

    add vercel frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin:["http://localhost:5173", "http://localhost:3000"],
+  origin:["http://localhost:5173", "http://localhost:3000","https://job-sprint-opal.vercel.app"],
   credentials:true
 
 }))
